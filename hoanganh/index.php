54495ea2c258ad02a1a8d44a090c952c3956e9e3
hoangganhle <lehoanganh20297@gmail.com>
2019-08-01 11:32:04 +0700

    Update

--- hoanganh/index.php
+++ hoanganh/index.php
@@ -7,6 +7,7 @@
 <body>
     <h1>Hello</h1>
     <h1>My name is Hoang Annyugugugusadasd</h1>
+<<<<<<< HEAD
     <h1>22 years oldasudhisahdhaisdhu</h1>
     <h1>22 years oldasudhisahdhaisdhu</h1>
     <h1>22 years oldasudhisahdhaisdhu</h1>
@@ -14,5 +15,14 @@
     <h1>22 years oldasudhisahdhaisdhu</h1>
     <h1>22 years oldasudhisahdhaisdhu</h1>
     tao conflic ne
+=======
+    <h1>22 years oldasudhisahdhaisdhu hahahha</h1>
+    <h1>22 years oldasudhisahdhaisdhu hahahha</h1>
+    <h1>22 years oldasudhisahdhaisdhu hahahha</h1>
+    <h1>22 years oldasudhisahdhaisdhu hahahha</h1>
+    <h1>22 years oldasudhisahdhaisdhu hahahha</h1>
+    <h1>22 years oldasudhisahdhaisdhu hahahha</h1>
+
+>>>>>>> Update
 </body>
 </html>
--- phuong/test.php
+++ phuong/test.php
@@ -1,8 +1,13 @@
 <?php
     echo "Hello world";
+
     asdasd
     adasdsa
     date_isodate_set()date_date_set()asdasdasdas
     do {
 
     } while (asdasdasdasdsad);
+
+echo "Hoangg Anh Le";
+
+echo "hjkhkhiusahkdjhasidhasidhiad";
